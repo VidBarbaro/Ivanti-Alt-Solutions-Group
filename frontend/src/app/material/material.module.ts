07fcba4a78886d9a317c685d1c78f50f32ea0ae1
Vid Barbaro <i459559@fhict.nl>
2021-10-12 21:32:01 +0200

    Add initial frontend with v1 navbar

---
+++ frontend/src/app/material/material.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+
+const MaterialComponents = [
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+]
+
+@NgModule({
+  imports: [
+    MaterialComponents
+  ],
+  exports: [
+    MaterialComponents
+  ]
+})
+export class MaterialModule { }
