1d550a7a2c2f647022f38e8db4f3f96ac3d3d68f
Mitran,Matei M.C <m.mitran@student.fontys.nl>
2021-11-18 17:31:27 +0100

    Sequencing for ID

--- backend/src/main/java/com/ivantimarket/ivanti/controllers/UserController.java
+++ backend/src/main/java/com/ivantimarket/ivanti/controllers/UserController.java
@@ -4,6 +4,7 @@ import com.ivantimarket.ivanti.dto.NewUserDTO;
 import com.ivantimarket.ivanti.dto.UserDTO;
 import com.ivantimarket.ivanti.model.User;
 import com.ivantimarket.ivanti.repo.UserRepository;
+import com.ivantimarket.ivanti.service.SequenceGeneratorService;
 import com.ivantimarket.ivanti.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +16,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.SequenceInputStream;
 import java.net.URI;
 import java.util.List;
 
@@ -25,9 +27,12 @@ public class UserController {
 
     private final UserService userService;
 
+    private final SequenceGeneratorService sequenceGeneratorService;
 
-    public UserController(UserService userService) {
+
+    public UserController(UserService userService, SequenceGeneratorService sequenceGeneratorService) {
         this.userService = userService;
+        this.sequenceGeneratorService = sequenceGeneratorService;
     }
 
 //    @GetMapping("/users")
@@ -38,6 +43,7 @@ public class UserController {
     @PostMapping("/register")
     public ResponseEntity<UserDTO> saveUser(@RequestBody NewUserDTO userDTO) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
+        userDTO.setId(sequenceGeneratorService.generateSequence(User.SEQUENCE_NAME));
         return ResponseEntity.created(uri).body(userService.saveUser(userDTO));
     }
 
--- backend/src/main/java/com/ivantimarket/ivanti/model/User.java
+++ backend/src/main/java/com/ivantimarket/ivanti/model/User.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.stereotype.Service;
@@ -17,6 +18,9 @@ import java.util.Set;
 @Getter
 @Setter
 public class User {
+
+    @Transient
+    public static final String SEQUENCE_NAME = "users_sequence";
 
     @Id
     private long id;
@@ -50,7 +54,8 @@ public class User {
     public User() {
 
     }
-
+    public long getId() { return this.id;}
+    public void setId(long id) {this.id=id;}
     public String getEmail() {
         return email;
     }
