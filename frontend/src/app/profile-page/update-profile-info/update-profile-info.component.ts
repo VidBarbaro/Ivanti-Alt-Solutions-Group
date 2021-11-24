57ff1b599afb85181de1925dc10758868e565715
Vid Barbaro <i459559@fhict.nl>
2021-11-24 16:32:47 +0100

    Add profile page, update profile & reset password

---
+++ frontend/src/app/profile-page/update-profile-info/update-profile-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-update-profile-info',
+  templateUrl: './update-profile-info.component.html',
+  styleUrls: ['./update-profile-info.component.css']
+})
+export class UpdateProfileInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
