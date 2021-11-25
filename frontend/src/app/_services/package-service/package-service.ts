e99e8bbb860afba4422deb86dcd07582e867c729
Vid Barbaro <i459559@fhict.nl>
2021-11-25 14:41:22 +0100

    Add footer and frontend connection to receive packages from backend

---
+++ frontend/src/app/_services/package-service/package-service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpErrorResponse, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Package } from 'src/app/model/package';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class PackageService {
+    private host = environment.apiUrl;
+
+    constructor(private http: HttpClient) { }
+
+    public getPackages(): Observable<HttpResponse<Package[]>> {
+        return this.http.get<Package[]>(`${this.host}/api/packages`, { observe: 'response' });
+    }
+
+    //   public addPackage(formData: FormData): Observable<Package | HttpErrorResponse> {
+    //     return this.http.post<Package>(`${this.host}/user/add`, formData);
+    //   }
+
+    public addPackagesToLocalCache(packages: Package[]): void {
+        localStorage.setItem('packages', JSON.stringify(packages));
+    }
+
+
+}
---
+++ frontend/src/app/model/package.ts
@@ -0,0 +1,10 @@
+export class Package {
+    public id: number;
+    public title: string;
+    public creator: string;
+
+    constructor() {
+        this.title = '';
+        this.creator = '';
+    }
+}
