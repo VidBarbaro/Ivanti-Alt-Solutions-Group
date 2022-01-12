b71845d098332730389c05f015502de1b76907b3
anamarianitu <anamarianitu5@gmail.com>
2022-01-12 15:41:25 +0100

    Add change to security config

--- backend/src/main/java/com/ivantimarket/ivanti/security/SecurityConfig.java
+++ backend/src/main/java/com/ivantimarket/ivanti/security/SecurityConfig.java
@@ -45,7 +45,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.authorizeRequests().antMatchers("/api/login/**","/api/register/**", "/api/token/refresh/**", "/api/users/**", "/api/packages/get/**", "/api/packages/","/api/packages/download**","/user/login/**", "/api/packages/create",
                 "/api/packages/add-new-package", "/api/packages/add-version/{packageId}", "/api/packages/uploaded/{userId}", "/api/packages/versions/{packageId}", "/api/packages/system-requirements/{packageId}", "/api/packages/update-package",
-                "/api/packages/favourites/add", "/api/packages/favourites/check", "/api/packages/favourites/remove", "/api/review/create", "api/user/changeFirstTimeVar").permitAll();
+                "/api/packages/favourites/add", "/api/packages/favourites/check", "/api/packages/favourites/remove", "/api/review/create", "/api/user/changeFirstTimeVar").permitAll();
 
         http.authorizeRequests().antMatchers(GET, "/api/user/**").hasAnyAuthority("ROLE_USER");
         http.authorizeRequests().antMatchers(POST, "/api/user/update","/api/user/update/password").hasAnyAuthority("ROLE_CUSTOMER", "ROLE_CONTENT_CREATOR");
